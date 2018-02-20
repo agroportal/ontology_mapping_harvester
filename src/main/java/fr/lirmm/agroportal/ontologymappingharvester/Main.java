644c94ff6dc96bb8c0c6fce0b676765afb56bfe3
abrahao <elcio.abrahao@lirmm.fr>
2018-02-20 16:18:22 +0100

    Load ANAEETHES and parse skos

--- src/main/java/fr/lirmm/agroportal/ontologymappingharvester/Main.java
+++ src/main/java/fr/lirmm/agroportal/ontologymappingharvester/Main.java
@@ -1,11 +1,10 @@
 package fr.lirmm.agroportal.ontologymappingharvester;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.*;
 
 import java.io.File;
+import java.util.Set;
 
 public class Main {
 
@@ -16,20 +15,64 @@ public class Main {
 
         OWLOntologyManager man = OWLManager.createOWLOntologyManager();
         OWLOntology oA = null;
-        OWLOntology oB = null;
 
-        File fileA = new File("/home/abrahao/data/downloads/pizza.owl");
-        File fileB = new File("/home/abrahao/data/downloads/tutorial_local.owl");
+        int countClasses=0;
+        int countIndividuals=0;
+
+        File fileA = new File("/home/abrahao/data/downloads/thesaurus_pour_agroportal.rdf");
+
 
         try {
 
             oA = man.loadOntologyFromOntologyDocument(fileA);
-            System.out.println(oA);
-            oB = man.loadOntologyFromOntologyDocument(fileB);
+//            System.out.println(oA);
+//            oA.logicalAxioms().forEach(System.out::println);
 
-            System.out.println("-------------------------------------------------------------");
+            System.out.println("ONTOLOGY:"+oA.toString());
 
-            System.out.println(oB);
+            System.out.println("----------------------------------------------------------");
+
+            System.out.println("Classes");
+            for (OWLClass c : oA.getClassesInSignature()) {
+                System.out.println(c.toString());
+                countClasses++;
+            }
+            System.out.println("Total Classes: "+countClasses);
+            System.out.println("----------------------------------------------------------");
+
+            System.out.println("Filter - Individuals");
+            for (OWLNamedIndividual ind : oA.getIndividualsInSignature()) {
+
+
+                Set<? extends OWLAxiom> axioms = oA.getAxioms(ind);
+                for (OWLAxiom ax : axioms) {
+                    System.out.println("Axiom " + ax);
+                }
+                axioms = oA.getAnnotationAssertionAxioms(ind.getIRI());
+                for (OWLAxiom ax : axioms) {
+                    System.out.println("Annotation Assertion Axiom " + ax);
+                }
+                // (<http://www.w3.org/2004/02/skos/core#exactMatch>
+
+
+                axioms = oA.getDataPropertyAssertionAxioms(ind);
+                for (OWLAxiom ax : axioms) {
+                    System.out.println("Data Property Assertion Axiom " + ax);
+
+                }
+                axioms = oA.getObjectPropertyAssertionAxioms(ind);
+                for (OWLAxiom ax : axioms) {
+                    System.out.println("Object Property Assertion Axiom " + ax);
+                }
+
+
+
+                countIndividuals++;
+
+            }
+            System.out.println("Total Individuals: "+countIndividuals);
+            System.out.println("----------------------------------------------------------");
+
         } catch (OWLOntologyCreationException e) {
             e.printStackTrace();
         }

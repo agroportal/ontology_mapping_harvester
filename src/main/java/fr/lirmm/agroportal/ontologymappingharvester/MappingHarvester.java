5f9c2e464070491b7881b82065baf6b423e88731
elcioabrahao <elcio.abrahao@gmail.com>
2018-04-09 16:05:24 +0200

    Correct bug on command length

--- src/main/java/fr/lirmm/agroportal/ontologymappingharvester/MappingHarvester.java
+++ src/main/java/fr/lirmm/agroportal/ontologymappingharvester/MappingHarvester.java
@@ -29,7 +29,7 @@ public class MappingHarvester {
 
             command = args[0].replace("-","");
             if(args.length==1){
-                if(command.indexOf("-")==-1 || command.length()==1){
+                if(command.indexOf("-")==-1 || command.length()==0){
                     System.out.println();
                     System.out.println("Attention: No parameters provided! Nothing to do!");
                     printHelp();

90d168aab0c0fcfa1088017d93a8c214a0760886
elcioabrahao <elcio.abrahao@gmail.com>
2018-08-23 11:43:06 +0200

    Minor fixes.

--- src/main/java/fr/lirmm/agroportal/ontologymappingharvester/meatlab/Main.java
+++ src/main/java/fr/lirmm/agroportal/ontologymappingharvester/meatlab/Main.java
@@ -7,14 +7,9 @@ public class Main {
         Parser parser = new Parser();
         parser.execute("FOODON.json.tmp", "foodon","DATABASE_ANSES");
 
-        Parser parser2 = new Parser();
-        parser2.execute("GACS.json.tmp", "gacs","DATABASE_ANSES");
-
         Parser parser3 = new Parser();
         parser3.execute("FOODON.json.tmp", "foodon","DATABASE_USDA");
 
-        Parser parser4 = new Parser();
-        parser4.execute("GACS.json.tmp", "gacs","DATABASE_USDA");
 
 
     }
